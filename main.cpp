41ef4591db275961a6950b8f48b9fb4f26de7fa6
Martin Milde <sidewindernor@gmail.com>
2024-02-11 00:37:16 +0100

    Finished the basic setup and loaded the map.

--- main.cpp
+++ main.cpp
@@ -2,21 +2,28 @@
 
 int main()
 {
-    //These numbers are wrong. Update later.
-    int windowX = 800;
-    int windowY = 600;
-
+    //Window Dimensions
+    const int windowX {384};
+    const int windowY {384};
+    
     InitWindow(windowX, windowY, "RPG");
-
     SetTargetFPS(60);
+
+    Texture2D map = LoadTexture("nature_tileset/worldMap.png");
 
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
         ClearBackground(WHITE);
+
+        Vector2 mapCoordinates {0.0, 0.0};
+        DrawTextureEx(map, mapCoordinates, 0.0, 4.0, WHITE);
+
         EndDrawing();
     }
 
+
+    UnloadTexture(map);
     CloseWindow();
 
 }
