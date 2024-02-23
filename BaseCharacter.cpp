55258776285a165605336705a464e226dd5e95e4
Martin Milde <sidewindernor@gmail.com>
2024-02-23 23:28:25 +0100

    Moved methods and functions from Enemy and Character over to BaseCharacter

--- BaseCharacter.cpp
+++ BaseCharacter.cpp
@@ -2,5 +2,16 @@
 
 BaseCharacter::BaseCharacter()
 {
-    
+    width = texture.width / maxFrames;
+    height = texture.height;
+}
+
+void BaseCharacter::undoMovement()
+{
+    worldPos = worldPosLastFrame;
+}
+
+Rectangle BaseCharacter::getCollisionRec()
+{
+    return Rectangle {screenPos.x, screenPos.y, scale * width, scale * height};
 }
--- Character.cpp
+++ Character.cpp
@@ -58,13 +58,3 @@ void Character::tick(float deltaTime)
     Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
     DrawTexturePro(texture, source, dest, Vector2{}, 0.0f, WHITE);
 }
-
-void Character::undoMovement()
-{
-    worldPos = worldPosLastFrame;
-}
-
-Rectangle Character::getCollisionRec()
-{
-    return Rectangle {screenPos.x, screenPos.y, scale * width, scale * height};
-}
